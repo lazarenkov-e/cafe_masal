3a213d8da32a8145b3ed259af0182fea735fa6d8
Евгений Лазаренков <lazarenkov88@gmail.com>
2022-12-04 13:28:55 +0300

    Cafe Masal 4 dec

--- cafe/pages/views.py
+++ cafe/pages/views.py
@@ -3,7 +3,7 @@ from pages.models import Worker
 
 
 def index_page(request):
-    # Worker.objects.get(id=3).delete()
+    # Worker.objects.get(id=3).delete() [j[j]]
     all_workers = Worker.objects.all()
     for i in all_workers:
         print(i.name, i.lastname, i.salary, i.id)
